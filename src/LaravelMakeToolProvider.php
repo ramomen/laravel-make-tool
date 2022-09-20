9d9172db8c91d76d3b6b0125384d3e43339fb8b5
Ramazan Değirmenci <45834008+ramomen@users.noreply.github.com>
2022-09-20 11:15:49 +0300

    🎉heyyo

---
+++ src/LaravelMakeToolProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ramomen\LaravelMakeTool;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMakeToolProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->commands(MakeTool::class);
+    }
+}
